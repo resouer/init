ba31652f3187b39d39bf82561b0fbf6451dd7d33
Harry Zhang <harryzhang@zju.edu.cn>
2015-10-10 16:19:01 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
 
 # install git
+apt-get update
 apt-get install -y git
 git config --global user.name "Harry Zhang"
 git config --global user.email "harryzhang@zju.edu.cn"
