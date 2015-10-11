93f8da8fbc0bbb72437377fc66afec20b2b99562
Harry Zhang <harryzhang@zju.edu.cn>
2015-10-11 16:15:32 +0800

    Add docker isntall

--- init.sh
+++ init.sh
@@ -53,3 +53,6 @@ apt-get install -y curl
 curl -L  https://github.com/coreos/etcd/releases/download/v2.2.0/etcd-v2.2.0-linux-amd64.tar.gz -o etcd-v2.2.0-linux-amd64.tar.gz
 tar xzvf etcd-v2.2.0-linux-amd64.tar.gz
 cp etcd-v2.2.0-linux-amd64/etcd /usr/local/bin
+
+# install docker
+wget -qO- https://get.docker.com/ | sh
